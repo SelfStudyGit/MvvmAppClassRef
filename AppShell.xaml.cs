2befef7245666466a1855efceb9c8b3675ccbb16
kh <hirose.k.ad@gmail.com>
2023-04-28 06:10:30 +0900

    init

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MvvmAppClassRef;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MvvmAppClassRef;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
